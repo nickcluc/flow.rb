04430c422c2491f7041722e300773da56eb53f94
nickcluc <nclucas01@gmail.com>
2014-11-24 15:59:34 -0500

    readability of server.rb

--- server.rb
+++ server.rb
@@ -3,9 +3,21 @@ require 'sinatra'
 
 def get_tracks(input)
   time = input.to_i * 60000
-  client = SoundCloud.new(:client_id => '71d297a0573a3d0e909f86ed41f160c5')
-  playlists = client.get('/tracks', :q => 'house chill', :duration => { :from => time - 300000, :to => time + 300000}, :limit => 30)
+
+  client = SoundCloud.new(
+    :client_id => '71d297a0573a3d0e909f86ed41f160c5'
+  )
+  
+  playlists = client.get(
+    '/tracks',
+    :q => 'house chill',
+    :duration =>{
+      :from => time - 300000, :to => time + 300000},
+    :limit => 30
+  )
+
   uri = playlists.sample["uri"]
+
   uri
 end
 
